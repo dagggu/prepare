9615905fa2c1dbdfcb5d2a15b869d1117008eb0c
Daria Toropova <daria.toropova@stud.uni-heidelberg.de>
2023-03-25 19:13:51 +0100

    Add some more training code

--- errors.cc
+++ errors.cc
@@ -31,3 +31,50 @@ int main() {
     cout << sort(3.5, 6.0) << endl;
     cout << sort(5, 4) << endl;
 }
+
+class Animal {
+public:
+    virtual void sound() = 0;
+};
+
+class Cat : public Animal {
+public:
+    void sound() {
+        cout << "Meow!" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void sound() {
+        cout << "Woof!" << endl;
+    }
+};
+
+int main() {
+    Cat cat;
+    Dog dog;
+
+}
+
+class Vehicle {
+public:
+    void brand();
+    int year(int x);
+    void bipbup();
+};
+
+class BMW : public Vehicle {
+public:
+    void brand() { cout << "It's a BMW!" << endl; }
+    int year(int x) { return x; }
+    void bipbup() { cout << "I make a bip dop dop!" << endl; }
+};
+
+int main() {
+    BMW car;
+
+    car.brand();
+    cout << "I'm a " << car.year(20) << " years old!" << endl;
+    car.bipbup();
+}
