518698df0eed49e98c08f91257aa23de02be643b
Daria Toropova <daria.toropova@stud.uni-heidelberg.de>
2023-04-04 09:58:59 +0200

    Add some new comments for better understanding

--- student.cc
+++ student.cc
@@ -3,30 +3,39 @@
    von Schülern enthält. Implementieren Sie eine Methode, um den durchschnittlichen 
    Notendurchschnitt der Klasse zu berechnen. */
 
+// import necessary libraries
 #include <string>
 #include <vector>
 #include <iostream>
 
 using namespace std;
 
+// define class Student
 class Student {
 public:
     Student(string name, int age, float mark) : name_(name), age_(age), mark_(mark) { }
+    // get private name
     string getName() const { return name_; }
+    // get private age
     int getAge() const { return age_; }
+    // get private mark
     float getMark() const { return mark_; }
+// define got private variables
 private:
     string name_;
     int age_;
     float mark_;
 };
 
+// define class Class
 class Class {
 public:
+    // add student to private students vector
     void addStudent(const Student& student) {
         students_.push_back(student);
     }
 
+    // define a method to get an average mark of all students
     float getAverageMark() const {
         if (students_.empty()) {
             return 0;
@@ -38,15 +47,20 @@ public:
         return sum / students_.size();
     }
 private:
+    // declare private students vector
     std::vector<Student> students_;
 };
 
 int main() {
+    // declare an object klasse of Class
     Class klasse;
 
+    // Add students to klasse
     klasse.addStudent(Student("Alice", 18, 3.8));
     klasse.addStudent(Student("Bob", 19, 3.5));
     klasse.addStudent(Student("Charlie", 20, 3.7));
+
+    // get an average mark of the added students
     cout << "Average Mark: " << klasse.getAverageMark() << endl; // Output: Average GPA: 3.66667
 
     return 0;
